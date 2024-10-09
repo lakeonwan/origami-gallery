a39961d63e2c43a2e0ed3ff15953739ebd863f49
Lake on Wan <lakeonwan@gmail.com>
2024-10-09 18:49:18 +0200

    fix path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/origami-gallery/' // Change 'origami-gallery' to match your repo name
+      : '/'
 })
